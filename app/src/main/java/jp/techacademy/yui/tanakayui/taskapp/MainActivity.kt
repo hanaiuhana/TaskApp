3729240b8a1afa8b4817924f86d96f1ec79bc9f4
hanaiuhana <hana.iu.hana@gmail.com>
2020-08-11 20:23:21 +0900

    質問：課題→アプリが落ちる(データベース以降？)

--- app/src/main/java/jp/techacademy/yui/tanakayui/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yui/tanakayui/taskapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package jp.techacademy.yui.tanakayui.taskapp
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.RealmChangeListener
 import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
+const val EXTRA_TASK = "jp.techacademy.taro.kirameki.taskapp.TASK"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mTaskAdapter: TaskAdapter
@@ -24,8 +31,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            startActivity(intent)
         }
 
         // Realmの設定
@@ -37,18 +44,59 @@ class MainActivity : AppCompatActivity() {
         mTaskAdapter = TaskAdapter(this@MainActivity)
 
         // ListViewをタップしたときの処理
-        listView1.setOnItemClickListener { parent, view, position, id ->
+        listView1.setOnItemClickListener { parent, _, position, _ ->
             // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         // ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
             // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this@MainActivity)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendintIntent = PendingIntent.getBroadcast(
+                    this@MainActivity,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendintIntent)
+
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
             true
         }
 
+
+
         // アプリ起動時に表示テスト用のタスクを作成する
-        addTaskForTest()
+        //addTaskForTest()
 
         reloadListView()
     }
